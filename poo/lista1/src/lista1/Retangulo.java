a769bae5daef47de797dec24f92200346f01622d
Alcides Augusto Bezerra Neto <alcalcides@hotmail.com>
2019-03-18 19:10:11 -0300

    antes de criar valores default

--- poo/lista1/src/lista1/Retangulo.java
+++ poo/lista1/src/lista1/Retangulo.java
@@ -1,5 +1,24 @@
 package lista1;
 
 public class Retangulo {
-
+	private double largura;
+	private double altura;
+	
+	public Retangulo(double largura, double altura) {
+		this.largura = largura;
+		this.altura = altura;
+	}
+	
+	public double getLargura() {
+		return largura;
+	}
+	public void setLargura(double largura) {
+		this.largura = largura;
+	}
+	public double getAltura() {
+		return altura;
+	}
+	public void setAltura(double altura) {
+		this.altura = altura;
+	}
 }
