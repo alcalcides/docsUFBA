44c0ec9a43441212014dab17ace86382b9b7bd69
alcalcides <alcalcides@hotmail.com>
2019-04-11 23:55:51 -0300

    Exercício 1 ok

--- poo/lista2/src/br/ufba/mata55/lista2/exercicio1/aluno.java
+++ poo/lista2/src/br/ufba/mata55/lista2/exercicio1/aluno.java
@@ -1,33 +1,43 @@
 package br.ufba.mata55.lista2.exercicio1;
 
-public class aluno {
-	private String nome;
+public class Aluno {
+	private final String nome;
 	private double nota;
 	
-	public aluno(String nome) {
-		super();
+	public Aluno(String nome) {
 		this.nome = nome;
 	}
 
-	public aluno(String nome, double nota) {
-		super();
+	public Aluno(String nome, double nota) {
 		this.nome = nome;
-		this.nota = nota;
+		this.setNota(nota);
 	}
 
 
 	public String getNome() {
 		return nome;
 	}
-	public void setNome(String nome) {
-		this.nome = nome;
-	}
+//	public void setNome(String nome) {
+//		this.nome = nome;
+//	}
 	public double getNota() {
 		return nota;
 	}
 	public void setNota(double nota) {
-		this.nota = nota;
+		if (nota >= 0.0 && nota <= 10.0)
+			this.nota = nota;
+		else 
+			System.out.println("Nota invalida");
 	}
 	
+	public static void main(String[] args) {
+		Aluno std1 = new Aluno("Alcides", 7);
+		System.out.println(std1.nome);
+		System.out.println(std1.nota);
+		Aluno std2 = new Aluno("Cido");
+		System.out.println(std2.nome);
+		System.out.println(std2.nota);
+		
+	}
 
 }
