77665dd6f65eb0993e5f259e1086886406ff97c5
Alcides Augusto Bezerra Neto <alcalcides@hotmail.com>
2019-04-23 18:31:39 -0300

    Início da implementação de biblioteca para listas encadeadas simples sem head

---
+++ algoritmos/filaCircular/fila.c
@@ -0,0 +1,7 @@
+#include <stdio.h>
+#include "fila.h"
+
+
+int main (){
+    return 0;
+}
---
+++ algoritmos/filaCircular/lista.h
@@ -0,0 +1,73 @@
+/*
+Implementação de lista encadeada simples 
+sem head
+*/
+
+#include <stdlib.h>
+
+typedef struct elto Elto;
+typedef struct list List;
+List* newListVazia();
+
+struct elto {
+    int id;
+    int numServ;
+    Elto* next;
+};
+
+struct list {
+    int numEltos;
+    Elto* firstElto;
+};
+
+Elto* newElto(int id, int numServ);
+Elto* delElto(Elto* ant);
+
+
+/*
+A função newElto recebe os valores que compõem um nó e
+cria um nó com esses valores e retorna um ponteiro para o nó criado
+*/
+Elto* newElto(int id, int numServ){
+    Elto* node = (Elto*)malloc(1*sizeof(Elto));
+    if(node){
+        node->id = id;
+        node->numServ = numServ;
+    }
+    return node;
+}
+
+/*
+A função delElto recebe um nó e exclui o nó logo à frente
+e retorna o ponteiro para esse nó anterior ou, 
+no caso de não haver nó à frente, ele exclui o próprio nó
+e retorna NULL
+*/
+Elto* delElto(Elto* ant){
+    if(ant->next == NULL){
+        free(ant);
+    }
+    else{
+        Elto* temp = ant->next->next;
+        free(ant->next);
+        ant->next = temp;
+    }
+    return ant;
+}
+
+/*
+A função newListVazia cria uma lista vazia e
+retorna o ponteiro para a lista criada
+*/
+List* newListVazia(){
+    List* lista = (List*)malloc(1*sizeof(List));
+    lista->numEltos = 0;
+    lista->firstElto = NULL;
+    return lista;
+}
+
+/*
+A função insereEltoLista recebe ponteiro do 
+elemento a ser inserido na lista, o ponteiro da lista
+e a posição e retorna...
+*/
